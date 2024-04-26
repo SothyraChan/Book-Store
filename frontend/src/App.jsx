b0a47c087097fd9f54320ca30d54149e23964e9f
Sothyra Chan <JuuJuu.q.cb@gmail.com>
2024-04-26 01:06:23 -0400

    third commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
-/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from './pages/ShowBooks';
+import EditBook from './pages/EditBooks';
+import DeleteBook from './pages/DeleteBooks';
+
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/books/create' element={<CreateBook />}/>
+      <Route path='/books/details/:id' element={<ShowBook />}/>
+      <Route path='/books/edit/:id' element={<EditBook />}/>
+      <Route path='/books/delete/:id' element={<DeleteBook />}/>
+    </Routes>
   )
 }
 

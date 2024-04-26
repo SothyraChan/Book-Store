aeb9bacc073d5b404fc50d9c24030d9dc092cf71
Sothyra Chan <JuuJuu.q.cb@gmail.com>
2024-04-26 02:27:27 -0400

    update home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import axios from "axios";
-import Spinner from "../components/Spinner";
-import { Link } from "react-router-dom";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import axios from 'axios';
+import Spinner from '/src/components/Spinner.jsx';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
   const [books, setBooks] = useState([]);
@@ -22,7 +22,6 @@ const Home = () => {
         setLoading(false);
       });
   }, []);
-
   return (
     <div className="p-4">
       <div className="flex justify-between items-center">
